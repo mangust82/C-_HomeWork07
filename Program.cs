002ac845134a1e38a679e776c6fd942f35f3dfb5
goloveshkin <mangust82@gmail.com>
2022-10-23 12:05:53 +0300

    Task50

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-Console.WriteLine("введите количество строк");
+/*Console.WriteLine("введите количество строк");
 int m  = int.Parse(Console.ReadLine());
 Console.WriteLine("введите количество столбцов");
 int n  = int.Parse(Console.ReadLine());
 double[,] MyArray = GetArray(m, n);
+PrintArray(MyArray);*/
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет. Например, задан массив:
+
+double[,] MyArray = GetArray(10, 10);
 PrintArray(MyArray);
+Console.WriteLine("Введите позицию элемента по строке");
+int m  = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите позицию элемента по столбцу");
+int n  = int.Parse(Console.ReadLine());
+PrintElement(m, n, MyArray);
+
+
 
 void PrintArray(double[,] matr)
 {
@@ -32,4 +45,11 @@ double[,] GetArray(int m, int n)
         }
     }
     return array;
+}
+
+void PrintElement(int x, int y, double[,] array)
+{
+    if (x <= array.GetLength(0) - 1  && x >= 0 && y <= array.GetLength(1) - 1 && y >= 0 )  
+    {Console.WriteLine(array[x,y]);}
+    else {Console.WriteLine("Элемента с заданной позицией нет");}
 }
